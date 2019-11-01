5551f2d84966e31a758fa8f6557f7f00fee4985d
Sam <super8989@gmail.com>
2019-10-31 21:20:45 -0400

    boilerplate for GithubUserInfo: Loading data with async function exercise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Timer from "./Timer";
 import ZenQuote from "./ZenQuote";
+import GithubUserInfo from "./GithubUserInfo";
 import "./App.css";
 
 function App() {
 	return (
 		<div className='App'>
-			<ZenQuote />
+			<GithubUserInfo username='facebook' />
+			<GithubUserInfo username='super8989' />
 		</div>
 	);
 }
---
+++ src/GithubUserInfo.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class GithubUserInfo extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Github User Info</h1>
+			</div>
+		);
+	}
+}
